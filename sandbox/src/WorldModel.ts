56a0152b5baffd7b4e3dbe070d89e59030a1233d
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-04-30 16:11:00 -0400

    Update WorldModel.ts

--- sandbox/src/WorldModel.ts
+++ sandbox/src/WorldModel.ts
@@ -1,1 +1,15 @@
+import display from "./display";
+import Snake from "./Snake";
 
+class WorldModel {
+  private snake: Snake;
+
+  constructor(snake: Snake) {
+    this.snake = snake;
+  }
+
+  public update(steps: number): void {
+    this.snake.move(steps);
+  }
+}
+export default WorldModel;
