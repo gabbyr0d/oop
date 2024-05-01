ca1fc94f8b0dcb0c77f588fde917bcf34b83b83d
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-04-30 20:17:39 -0400

    Create AvoidWallsPlayer.ts

---
+++ sandbox/src/AvoidWallsPlayer.ts
@@ -0,0 +1,28 @@
+import display from "./display";
+import Snake from "./Snake";
+import WorldModel from "./WorldModel";
+import SnakeController from "./SnakeController";
+import Player from "./Player";
+
+class AvoidWallsPlayer extends Player {
+  constructor(snakeController: SnakeController) {
+    super(snakeController);
+  }
+
+  public makeTurn() {
+    const snakePosition = this.sc.snakePosition;
+    const snakeDirection = this.sc.snakeDirection;
+
+    if (snakeDirection === -1 && snakePosition.x === 0) {
+      this.sc.turnSnakeRight();
+    } else if (
+      snakeDirection === 1 &&
+      snakePosition.x === this.sc.worldWidth - 1
+    ) {
+      // Avoid right wall going down
+      this.sc.turnSnakeLeft();
+    }
+  }
+}
+
+export default AvoidWallsPlayer;
