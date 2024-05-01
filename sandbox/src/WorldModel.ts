c40e768db986e80692dac102d1d7c46a4a9f6922
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-04-30 20:16:32 -0400

    Update WorldModel.ts

--- sandbox/src/WorldModel.ts
+++ sandbox/src/WorldModel.ts
@@ -3,13 +3,23 @@ import Snake from "./Snake";
 
 class WorldModel {
   private snake: Snake;
-
-  constructor(snake: Snake) {
+  private worldWidth: number;
+  private worldHeight: number;
+  constructor(snake: Snake, width: number, height: number) {
     this.snake = snake;
+    this.worldWidth = width;
+    this.worldHeight = height;
   }
 
   public update(steps: number): void {
     this.snake.move(steps);
   }
+  public get width(): number {
+    return this.worldWidth;
+  }
+  public get height(): number {
+    return this.height;
+  }
 }
 export default WorldModel;
+
