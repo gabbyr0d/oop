a50a864f808722eaf9224debcc51bfe71ab8eadc
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-05-02 18:15:23 -0400

    Update Snake.ts

--- sandbox/src/Snake.ts
+++ sandbox/src/Snake.ts
@@ -1,11 +1,14 @@
 import display from "./display";
 import Point from "./Point";
-class Snake {
+import Collidable from "./Collidable";
+import Actor from "./Actor";
+class Snake implements Collidable {
   private currentPosition: Point;
   private currentDirection: number;
   private startPosition: Point;
   private size: number;
   private currentParts: Point[];
+  private isCurrentlyActive: boolean;
   constructor(startPosition: Point, size: number, direction: number) {
     this.startPosition = startPosition;
     this.size = size;
@@ -13,6 +16,7 @@ class Snake {
     this.currentDirection = 1;
     this.currentParts = [];
     this.currentParts.push(startPosition);
+    this.isCurrentlyActive = true;
   }
   /**
    * @deprecated Use {turn}
@@ -62,13 +66,41 @@ class Snake {
       );
     }
   }
-  public didCollide(s: Snake): boolean {
-    return this.currentParts.some(
-      (part, index) => index !== 0 && s.currentParts.includes(part),
-    );
+  public didCollide(a: Actor): boolean {
+    if (this.type !== "snake") {
+      return this.position.equals(a.position());
+    } else {
+      return this.currentParts
+        .slice(1)
+        .some((part) => this.position.equals(part));
+    }
+  }
+  public update() {
+    this.move(1);
+  }
+  public die() {
+    this.isCurrentlyActive = false;
+  }
+  public grow() {
+    if (this.currentDirection === 1) {
+      this.currentParts.push(new Point(this.position.x, this.position.y - 1));
+    } else if (this.currentDirection === 4) {
+      this.currentParts.push(new Point(this.position.x, this.position.y + 1));
+    } else if (this.currentDirection === 2) {
+      this.currentParts.push(new Point(this.position.x + 1, this.position.y));
+    } else if (this.currentDirection === 3) {
+      this.currentParts.push(new Point(this.position.x - 1, this.position.y));
+    }
   }
   public get position(): Point {
-    return this.position;
+    return this.currentParts[0];
+  }
+  public get isActive() {
+    return this.isCurrentlyActive;
+  }
+
+  public get type() {
+    return "snake";
   }
 }
 export default Snake;
