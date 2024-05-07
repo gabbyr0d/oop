4370e3b75b1fa9b35ee1c49e2573aecf4a3bf8ec
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-05-07 17:58:51 -0400

    Update CanvasWorldView.ts

--- sandbox/src/CanvasWorldView.ts
+++ sandbox/src/CanvasWorldView.ts
@@ -1,6 +1,8 @@
 import IWorldView from "./IWorldView";
 import WorldModel from "./WorldModel";
 import Snake from "./Snake";
+import Actor from "./Actor";
+import Point from "./Point";
 class CanvasWorldView implements IWorldView {
   private scalingFactor: number;
   private worldCanvas: HTMLCanvasElement;
@@ -14,10 +16,10 @@ class CanvasWorldView implements IWorldView {
   public display(worldModel: WorldModel) {
     this.worldCanvas.width = worldModel.width * this.scalingFactor;
     this.worldCanvas.height = worldModel.height * this.scalingFactor;
-    for (const snake of worldModel.allSnakes) {
-      for (const part of snake.currentParts) {
+    for (const snake of worldModel.snakes) {
+      for (const part of snake.CurrentParts) {
         this.context.fillStyle = "green";
-        const snakePosition = worldModel.Snake.position;
+        const snakePosition = snake.position;
         const snakeSize = 1;
         this.context.fillRect(
           snakePosition.x * this.scalingFactor,
@@ -28,5 +30,29 @@ class CanvasWorldView implements IWorldView {
       }
     }
   }
+  public ScaleFactor(): number {
+    return this.scalingFactor;
+  }
+  public update(actors: Actor[]) {
+    this.context.clearRect(
+      0,
+      0,
+      this.context.canvas.width,
+      this.context.canvas.height,
+    );
+    for (const actor of actors) {
+      if (typeof actor.type === "string" && actor.type === "snake") {
+      } else if (typeof actor.type === "string" && actor.type === "food") {
+        const position = actor.position();
+        const x = position.x * this.ScaleFactor();
+        const y = position.y * this.ScaleFactor();
+        this.context.fillStyle = "red";
+        this.context.fillRect(x, y, this.ScaleFactor(), this.ScaleFactor());
+      }
+    }
+  }
+  public dispose() {
+    document.body.removeChild(this.context.canvas);
+  }
 }
-export default CanvasWorldView;
+
