3bd0b0a3c6633378b405220b0fe7dffc963874cd
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-05-07 18:18:01 -0400

    Update SnakeSnakeCollisionHandler.ts

--- sandbox/src/SnakeSnakeCollisionHandler.ts
+++ sandbox/src/SnakeSnakeCollisionHandler.ts
@@ -1,6 +1,6 @@
 import CollisionHandler from "./CollisionHandler";
 import Snake from "./Snake";
-class SnakeSnakeCollisionHandler implements CollisionHandler {
+class SnakeSnakeCollisionHandler implements CollisionHandler<Snake, Snake> {
   public applyAction(snake1: Snake, snake2: Snake) {
     snake1.die();
   }
