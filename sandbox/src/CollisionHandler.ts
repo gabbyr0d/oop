3fb4675ffbd31bb5b656ac947e87459a0c92402e
gabbyr0d <113070474+gabbyr0d@users.noreply.github.com>
2024-05-07 17:59:28 -0400

    Update CollisionHandler.ts

--- sandbox/src/CollisionHandler.ts
+++ sandbox/src/CollisionHandler.ts
@@ -1,8 +1,8 @@
 import display from "./display";
 import Snake from "./Snake";
 import Actor from "./Actor";
-interface CollisionHandler {
-  applyAction(actor: Actor, actor2: Actor): void;
+interface CollisionHandler<T, U> {
+  applyAction(actor: T, actor2: U): void;
 }
 
 export default CollisionHandler;
